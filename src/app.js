95449026e0cf743b33da4b3c78cf7f99e2c7fb26
S DILLIRAJ <132550822+Dilliraj-S@users.noreply.github.com>
2026-05-28 10:21:18 +0530

    Simplify dotenv configuration in app.js

--- src/app.js
+++ src/app.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const cors = require('cors');
 const path = require('path');
-require('dotenv').config({ path: path.join(__dirname, '../.env') });
+require('dotenv').config();
 
 const { syncDB } = require('./models');
 const adminRoutes = require('./routes/adminRoutes');
